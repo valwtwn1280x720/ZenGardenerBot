1e9e77d90b90d652ab83d062ff777fe2307441c2
valwntwn1280x720 <161889365+valwtwn1280x720@users.noreply.github.com>
2024-03-02 22:27:34 -0300

    Update Fumigar.py

--- Fumigar.py
+++ Fumigar.py
@@ -5,28 +5,31 @@ import time
 class Fumigador:
     def __init__(self) -> None:
         
-        #Definicion de rutas
+        #Definicion de rutas // Routes definition
         self.ruta_fumigar_planta = r"C:\Users\usuario\Desktop\Thingsss\ZenGardenerBot\images\fumigate_plant.png"
         self.ruta_fumigar = r"C:\Users\usuario\Desktop\Thingsss\ZenGardenerBot\images\fumigate.png"
         
     #Funcion para agarrar el fumigador
+    #Function for grabbing the pesticide
     def grab_fumigador(self):
-        #almacena posicion
-        needf = reconocer_img(self.ruta_fumigar, confianza=0.5)
+        #almacena posicion / contains position
+        needf = reconocer_img(self.ruta_fumigar, confianza=0.5) #this one fails a lot, i don't know why
         
-        if needf is not None: #click en posicion
+        if needf is not None: #click en posicion / click on position
             moverse_posicion(needf,clicar="si",duracion=1.0)
     
     def fumigar(self):
-        #almacena posicion
+        #almacena posicion / contains position
         need_fumigate = reconocer_img(self.ruta_fumigar_planta, confianza=0.85)
             
-        if need_fumigate: #click en posicion
+        if need_fumigate: #click en posicion / clicks on position
             print(f"Necesidad de fumigar en: {need_fumigate}")
             self.grab_fumigador()
             moverse_posicion(need_fumigate,clicar="si")
+            #time for the action to finish
             time.sleep(1)
         #si no se encuentra se muestra el mensaje
+        #If isn't found prints the message
         else:
             print("No se necesita fumigar")
             print("<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>")
