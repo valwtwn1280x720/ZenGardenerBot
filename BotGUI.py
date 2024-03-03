5ded3bf687c06ab184c0f2269656096d4735daa0
valwntwn1280x720 <161889365+valwtwn1280x720@users.noreply.github.com>
2024-03-02 22:13:41 -0300

    Update BotGUI.py

--- BotGUI.py
+++ BotGUI.py
@@ -3,8 +3,10 @@ import threading
 from customtkinter import *
 from MainBot import ZenGardenBot
 
+#define the zengardenbot
 ZGB = ZenGardenBot()
 
+#create the class BotWin that is the GUI for the bot (it still uses the console to provide the information)
 class BotWin(CTk):
     def __init__(self, mainbot):
         super().__init__()
@@ -27,6 +29,7 @@ class BotWin(CTk):
         self.intro_buttons_Frame.pack(side=BOTTOM)
         
         #labels
+        #You can traduce this if you want but isn't neccesary, i didn't know what to put in the window
         self.instrucciones ='''
 Instrucciones:
 1_. Este programa se compone de 4 bots:
@@ -47,7 +50,7 @@ ya abierto en el jardin Zen'''
                                             anchor=W,font=("Calibri",16))
         self.instrucciones_label.pack(side=LEFT,pady=7)
         
-        #botones
+        #botones, buttons
         self.start_stop_button = CTkButton(self.intro_buttons_Frame,command=self.change_state,
                                            text="Comenzar",fg_color="Green",hover=False)
         self.start_stop_button.pack(side="left",padx=5,pady=10)
@@ -55,13 +58,15 @@ ya abierto en el jardin Zen'''
         self.options_button = CTkButton(self.intro_buttons_Frame,text="Opciones",
                                         command=self.open_options,hover=False)
         self.options_button.pack(side="left",padx=5,pady=10)
-        
+
+    #Starts the loop from MainBot (solution given by chatgpt, didn't know how to do it)
     def start_botloop(self):
         self.botloop_thread = threading.Thread(target=self.mainbot.gardenloop)
         self.botloop_thread.start()
         
     def change_state(self):
         #Convierte bot_isrunning en true si es false y viceversa
+        #Convert bot_isrunning to true if false and vice versa
         if self.bot_isrunning == False:
             self.bot_isrunning = True
             
@@ -73,41 +78,44 @@ ya abierto en el jardin Zen'''
             self.start_stop()
             
         # Si está corriendo, establece el botón a rojo con el texto "Detener"
+        # If it is running, set the button to red with the text "Stop"
         if self.bot_isrunning:
             change = lambda: self.start_stop_button.configure(text="Detener",fg_color="Red")
             self.start_stop_button.after(100, change)
-            #desactiva el boton de opciones
+            #desactiva el boton de opciones // deactivate the options button
             self.options_button.configure(state="disabled")
         else:
             change = lambda: self.start_stop_button.configure(text="Comenzar",fg_color="Green")
             self.start_stop_button.after(100, change)
-            #lo activa si no se ejecuta el bot
+            #lo activa si no se ejecuta el bot // activates it if the bot is not running
             self.options_button.configure(state="normal")
-        
+    #if bot_isrunning is true it sets to 1 mainbot.running_loop for that loop to start    
     def start_stop(self):
         if self.bot_isrunning == True:
             self.mainbot.running_loop = 1
             self.start_botloop()
         else:
             self.mainbot.running_loop = 0
-        
+    #Opens the option panel if the bot isn't running    
     def open_options(self):
         if self.bot_isrunning == False:
             self.options_panel = self.OptionWin(master=self,botcc=self.mainbot.BotCC)
         else:
             pass
-        
+    #This exists because before it if you close the main window without stopping the loop it would be still running    
     def on_closing(self):
         self.mainbot.running_loop = 0
         self.destroy()
         sys.exit()
         
+    #Creates a class for the options window (didn't know if make this outside the class or as a function)    
     class OptionWin(CTkToplevel):
+        #initiates with the parameters master and botcc (coin collecter)
         def __init__(self, master,botcc):
             super().__init__()
             self.botcc = botcc
             
-            #ventana
+            #ventana // window
             self.title("Opciones")
             self.iconbitmap(master.icon)
         
@@ -116,6 +124,7 @@ ya abierto en el jardin Zen'''
             self.options_frame.pack(side=TOP,pady=10)
 
             #boton de aplicar y cancelar
+            #apply and cancel button
         
             self.opt_aplicar = CTkButton(self, text="Aplicar",command=self.aplicar,
                                      fg_color="Green")
@@ -125,32 +134,34 @@ ya abierto en el jardin Zen'''
                                       fg_color="Red",command=self.withdraw)
             self.opt_cancelar.pack(side=LEFT,pady=5,padx=10)
         
-            #opciones
+            #opciones // options
             
             #oportunidades de CoinCollecter (opr_cc)
+            #number of opportunities of CC to find coins on the screen (variable names here are super messy, sorry)
             self.opr_cc_label = CTkLabel(self.options_frame,
                                     text="Oportunidades de Coin Collecter:")
             self.opr_cc_label.grid(row=0,column=0,padx=5,pady=15)
         
-            #lista de opciones/oportunidades
+            #lista de opciones/oportunidades // opportunities/options list
             self.opr_num = [5,10,15,20]
             self.opr_num_str = ["5","10","15","20"]
             self.opr_var = StringVar(self.options_frame,self.opr_num_str[0])
-            #menu para cambiar
+            #menu para cambiar / menu to change it
             self.opr_cc = CTkOptionMenu(self.options_frame,values=self.opr_num_str,
                                     variable=self.opr_var,fg_color="Grey",
                                     button_color="Black",width=50,height=20)
             self.opr_cc.grid(row=0,column=1,padx=5)
             
-        #Funciones
+        #Funciones // Functions
+        #applies the option selected above
         def opr_cc_guardado(self):
             oprn = self.opr_cc.get()
+            #don't know how to explain this, it just made sense to me
             for i in self.opr_num:
                 if int(oprn) == i:
                     self.botcc.coin_tries = i
-        
-        
-        
+
+        #applies all the changes (only 1 option for now) and closes the options panel
         def aplicar(self):
             self.opr_cc_guardado()
             self.withdraw()
@@ -158,7 +169,7 @@ ya abierto en el jardin Zen'''
         
         
         
-
+#I read this was a good practice so i write this in every single file i work on 
 if __name__ == "__main__":
     root = BotWin(ZGB)
     root.mainloop()
