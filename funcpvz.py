f2563c2fa250eddf86be43ebc0a1c065f7ca6649
valwntwn1280x720 <161889365+valwtwn1280x720@users.noreply.github.com>
2024-03-02 22:35:48 -0300

    Update funcpvz.py

--- funcpvz.py
+++ funcpvz.py
@@ -2,6 +2,7 @@ import pyautogui as pag
 import time
 
 #click en la posicion a la que se movió el cursos
+#click func
 def click(doble="no"):
     if doble=="si":pag.doubleClick()
     else:
@@ -10,7 +11,8 @@ def click(doble="no"):
         pag.mouseUp()
 
 #reconocer si la imagen se encuentra en pantalla
-def reconocer_coins(list_img):
+#Recognize multiple instances of an object on screen
+def reconocer_coins(list_img): #list of routes
     for ubicacion in list_img:
         try:
             ubicacion_coin = pag.locateCenterOnScreen(ubicacion, confidence=0.75)
@@ -21,12 +23,14 @@ def reconocer_coins(list_img):
         
 
 #moverse a una posicion especifica
+#Moves to the coords given, and clicks or no
 def moverse_posicion(coords, clicar="no", duracion=0.001):
     pag.moveTo(coords[0], coords[1], duration=duracion)
     if clicar=="si":
         click()
     else:pass
 
+#Only recognizes one image/ instance of an object on screen (for those who don't move)
 def reconocer_img(ruta,confianza):
     try:
         ubicacion = pag.locateCenterOnScreen(ruta, confidence=confianza)
