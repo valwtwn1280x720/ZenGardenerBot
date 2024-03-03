3734101896955dcc6b2d1a0d742092a5f101e9c5
valwntwn1280x720 <161889365+valwtwn1280x720@users.noreply.github.com>
2024-03-02 22:33:20 -0300

    Update Regar.py

--- Regar.py
+++ Regar.py
@@ -6,12 +6,14 @@ class Regador:
     def __init__(self) -> None:
         
         #Se define las rutas de las imagenes necesarias a encontrar
+        #Defines the images routes
         self.ruta_water = r"C:\Users\usuario\Desktop\Thingsss\ZenGardenerBot\images\water.png"
         self.ruta_regadera = r"C:\Users\usuario\Desktop\Thingsss\ZenGardenerBot\images\regadera.png"
     
-    #Funcion para agarrar la regadera
+    #Funcion para agarrar la regadera / grab the watering can
     def grab_regadera(self):
-        #busca la imagen y la posicion la almacena en need
+        #busca la imagen y la posicion la almacena en need 
+        #contains position
         need = reconocer_img(self.ruta_regadera,confianza=0.9)
         
         if need: #si need no es None se mueve y clickea en esa posicion
@@ -19,15 +21,18 @@ class Regador:
             
     def regar(self):
         #busca imagen y almacena su posicion
+        #contains position
         need_water = reconocer_img(self.ruta_water, confianza=0.85)
         
         #si se encuentra
+        #if its found
         if need_water:
             print(f"Agua Encontrada: {need_water}")
             self.grab_regadera()
             moverse_posicion(need_water,clicar="si")
             time.sleep(1)
         #si no se encuentra se muestra el mensaje
+        #if it isn't found
         else:
             print("No se necesita agua")
             print("<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>")
