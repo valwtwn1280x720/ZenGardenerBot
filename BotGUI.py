4de124eaad41362d7b2faf6b9c761e3663b7c9d6
valwntwn1280x720 <161889365+valwtwn1280x720@users.noreply.github.com>
2024-03-03 01:17:45 -0300

    Update BotGUI.py

--- BotGUI.py
+++ BotGUI.py
@@ -6,6 +6,65 @@ from MainBot import ZenGardenBot
 #define the zengardenbot
 ZGB = ZenGardenBot()
 
+#Creates a class for the options window (didn't know if make this outside the class or as a function)    
+class OptionWin(CTkToplevel):
+    #initiates with the parameters master and botcc (coin collecter)
+    def __init__(self, master,botcc):
+        super().__init__()
+        self.botcc = botcc
+        
+        #ventana // window
+        self.title("Opciones")
+        self.iconbitmap(master.icon)
+        
+        #frames
+        self.options_frame = CTkFrame(self,width=280,height=180)
+        self.options_frame.pack(side=TOP,pady=10)
+
+        #boton de aplicar y cancelar
+        #apply and cancel button
+        
+        self.option_aplicar = CTkButton(self, text="Aplicar",command=self.aplicar,
+                                     fg_color="Green")
+        self.option_aplicar.pack(side=LEFT,pady=5,padx=10)
+        
+        self.option_cancelar = CTkButton(self, text="Cancelar",
+                                      fg_color="Red",command=self.withdraw)
+        self.option_cancelar.pack(side=LEFT,pady=5,padx=10)
+        
+        #opciones // options
+            
+        #oportunidades de CoinCollecter (oppty_cc)
+        #number of opportunities of CC to find coins on the screen (variable names here are super messy, sorry)
+        self.oppty_cc_label = CTkLabel(self.options_frame,
+                                    text="Oportunidades de Coin Collecter:")
+        self.oppty_cc_label.grid(row=0,column=0,padx=5,pady=15)
+        
+        #lista de opciones/oportunidades // opportunities/options list
+        self.oppty_nums = [5,10,15,20]
+        self.oppty_nums_as_str = ["5","10","15","20"]
+        self.oppty_var = StringVar(self.options_frame,self.oppty_nums_as_str[0])
+        #menu para cambiar / menu to change it
+        self.oppty_cc_menu = CTkOptionMenu(self.options_frame,values=self.oppty_nums_as_str,
+                                    variable=self.oppty_var,fg_color="Grey",
+                                    button_color="Black",width=50,height=20)
+        self.oppty_cc_menu.grid(row=0,column=1,padx=5)
+            
+    #Funciones // Functions
+    #applies the option selected above
+    def oppty_cc_save(self):
+        oppty_n = self.oppty_cc_menu.get()
+        #don't know how to explain this, it just made sense to me
+        for number in self.oppty_nums:
+            if int(oppty_n) == number:
+                self.botcc.coin_tries = number
+
+    #applies all the changes (only 1 option for now) and closes the options panel
+    def aplicar(self):
+        self.oppty_cc_save()
+        self.withdraw()
+
+
 #create the class BotWin that is the GUI for the bot (it still uses the console to provide the information)
 class BotWin(CTk):
     def __init__(self, mainbot):
@@ -18,7 +77,6 @@ class BotWin(CTk):
         self.geometry("480x360")
         self.title("Zen Gardener")
         self.icon = r"C:\Users\usuario\Desktop\Thingsss\ZenGardenerBot\images\icon_gui.ico"
-        self.config_ico = r"" #useless, can't remember (I think it was the icon for the options panel)
         self.iconbitmap(self.icon)
         
         #frames
@@ -85,96 +143,35 @@ ya abierto en el jardin Zen'''
         if self.bot_isrunning:
             change = lambda: self.start_stop_button.configure(text="Detener",fg_color="Red")
             self.start_stop_button.after(100, change)
+            
             #desactiva el boton de opciones // deactivate the options button
             self.options_button.configure(state="disabled")
         else:
             change = lambda: self.start_stop_button.configure(text="Comenzar",fg_color="Green")
             self.start_stop_button.after(100, change)
+            
             #lo activa si no se ejecuta el bot // activates it if the bot is not running
             self.options_button.configure(state="normal")
             
-    #if bot_isrunning is true it sets to 1 mainbot.running_loop for that loop to start    
+    #if bot_isrunning is true it sets to 1 mainbot.is_running_loop for that loop to start    
     def start_stop(self):
         if self.bot_isrunning == True:
-            self.mainbot.running_loop = 1
+            self.mainbot.is_running_loop = True
             self.start_botloop()
         else:
-            self.mainbot.running_loop = 0
+            self.mainbot.is_running_loop = False
             
     #Opens the option panel if the bot isn't running    
     def open_options(self):
-        if self.bot_isrunning == False:
-            self.options_panel = self.OptionWin(master=self,botcc=self.mainbot.BotCC)
-        else:
-            pass
+        if self.bot_isrunning is False:
+            self.options_panel = OptionWin(master=self,botcc=self.mainbot.BotCC)
             
     #This exists because before it if you close the main window without stopping the loop it would be still running    
     def on_closing(self):
-        self.mainbot.running_loop = 0
+        self.mainbot.is_running_loop = False
         self.destroy()
         sys.exit()
-        
-    #Creates a class for the options window (didn't know if make this outside the class or as a function)    
-    class OptionWin(CTkToplevel):
-        #initiates with the parameters master and botcc (coin collecter)
-        def __init__(self, master,botcc):
-            super().__init__()
-            self.botcc = botcc
-            
-            #ventana // window
-            self.title("Opciones")
-            self.iconbitmap(master.icon)
-        
-            #frames
-            self.options_frame = CTkFrame(self,width=280,height=180)
-            self.options_frame.pack(side=TOP,pady=10)
-
-            #boton de aplicar y cancelar
-            #apply and cancel button
-        
-            self.opt_aplicar = CTkButton(self, text="Aplicar",command=self.aplicar,
-                                     fg_color="Green")
-            self.opt_aplicar.pack(side=LEFT,pady=5,padx=10)
-        
-            self.opt_cancelar = CTkButton(self, text="Cancelar",
-                                      fg_color="Red",command=self.withdraw)
-            self.opt_cancelar.pack(side=LEFT,pady=5,padx=10)
-        
-            #opciones // options
-            
-            #oportunidades de CoinCollecter (opr_cc)
-            #number of opportunities of CC to find coins on the screen (variable names here are super messy, sorry)
-            self.opr_cc_label = CTkLabel(self.options_frame,
-                                    text="Oportunidades de Coin Collecter:")
-            self.opr_cc_label.grid(row=0,column=0,padx=5,pady=15)
-        
-            #lista de opciones/oportunidades // opportunities/options list
-            self.opr_num = [5,10,15,20]
-            self.opr_num_str = ["5","10","15","20"]
-            self.opr_var = StringVar(self.options_frame,self.opr_num_str[0])
-            #menu para cambiar / menu to change it
-            self.opr_cc = CTkOptionMenu(self.options_frame,values=self.opr_num_str,
-                                    variable=self.opr_var,fg_color="Grey",
-                                    button_color="Black",width=50,height=20)
-            self.opr_cc.grid(row=0,column=1,padx=5)
-            
-        #Funciones // Functions
-        #applies the option selected above
-        def opr_cc_guardado(self):
-            oprn = self.opr_cc.get()
-            #don't know how to explain this, it just made sense to me
-            for i in self.opr_num:
-                if int(oprn) == i:
-                    self.botcc.coin_tries = i
-
-        #applies all the changes (only 1 option for now) and closes the options panel
-        def aplicar(self):
-            self.opr_cc_guardado()
-            self.withdraw()
-
-        
-        
-        
+                
 #I read this was a good practice so i write this in every single file i work on 
 if __name__ == "__main__":
     root = BotWin(ZGB)
