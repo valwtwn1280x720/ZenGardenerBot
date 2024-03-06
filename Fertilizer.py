d0c6a319b38ed0fd5114dcd094387c19627983e1
valwntwn1280x720 <161889365+valwtwn1280x720@users.noreply.github.com>
2024-03-05 22:04:42 -0300

    Add files via upload

---
+++ Fertilizer.py
@@ -0,0 +1,20 @@
+from funcpvz import * 
+import time
+
+class Fertilizador:
+    def __init__(self) -> None:
+        
+        self.ruta_fertilizer = r"images\fertilizer.png"
+        self.ruta_fertilize_plant = r"images\fertilize_plant.png"
+        
+        
+    def grab_fertilizer(self):
+        need = reconocer_img(self.ruta_fertilizer, confianza=0.8)
+        if need:
+            moverse_posicion(need, clicar="si")
+    
+    def fertilize(self):
+        need_fert = reconocer_img(self.ruta_fertilize_plant, confianza=0.9)
+        if need_fert:
+            self.grab_fertilizer()
+            moverse_posicion(need_fert,clicar="si")
